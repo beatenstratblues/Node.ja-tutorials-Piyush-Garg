630577b52a3ac8ed03f082f3ed72856c0ec8c002
Jatin Singh <singh.jatin609@gmail.com>
2024-07-02 12:27:33 +0530

    todays changes

--- hello/index.js
+++ hello/index.js
@@ -1,12 +1,30 @@
 const http= require("http");
 const fs=require("fs");
-
-const log=`${Date.now()} A new user has looged in\n`;
+const url=require("url");
 
 const myserver= http.createServer((req,res)=>{
+    if (req.url==="/favicon.ico") return res.end();
+    const log=`${Date.now()} : ${req.url} A new user has looged in\n`;
+    const myurl=url.parse(req.url,true);
+    console.log(myurl);
+
     fs.appendFile("./userLogs.txt",log,(err)=>{
-        res.end("Hello!!! this is my server!!!");
+        console.log("A log has Been added");
     });
+    switch(myurl.pathname) {
+        case "/":{
+            res.end("This is the HomePage");
+            break;  
+        } 
+        case "/About":{
+            res.end("This Server Belongs to Jatin Singh");
+            break;
+        } 
+        case "/Contact":{
+            res.end("Contact me at : singh.jatin609@gmail.com");
+            break;
+        } 
+    }
 });
 
 
